66db5d9c30349b515e6ceff69e404b791fb44ec0
TDKA <teodor.kobilarov@gmail.com>
2021-12-09 12:02:12 +0100

    ..

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,7 @@ import firebase from 'firebase/compat/app'
 import 'firebase/compat/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAKwH_fdD_I23VSj2VUWQ1_aIvYRtH7_3w",
-  authDomain: "vue-firebase-14c6b.firebaseapp.com",
-  projectId: "vue-firebase-14c6b",
-  storageBucket: "vue-firebase-14c6b.appspot.com",
-  messagingSenderId: "686250184137",
-  appId: "1:686250184137:web:3b52883d3a423ecd792c18"
+
 };
 
 // init firebase
